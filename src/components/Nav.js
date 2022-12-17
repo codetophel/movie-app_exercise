37967aa0e796b8ecfbfeb50bc784696dac324431
codetophel <nicodemusakande@gmail.com>
2022-12-17 20:02:38 +0100

    modified:   src/components/Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <div className='sticky top-0 z-0 p-4 bg-[#191919] text-center lg:text-left'>
-      <h1 className='text-white p-2 ml-5'>MyMovieApp</h1>
+    <div className='sticky top-0 z-0 p-4 bg-[#191919] !text-center lg:text-left'>
+      <h1 className='text-white border-2 w-fit p-2 ml-5'>MyMovieApp</h1>
     </div>
   );
 };
